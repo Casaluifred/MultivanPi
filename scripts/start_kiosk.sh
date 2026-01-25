d50bbed7351c429f148679f4934d2312a84d92ea
Casaluifred <113069241+Casaluifred@users.noreply.github.com>
2026-01-25 21:08:41 +0100

    Update start_kiosk.sh

--- scripts/start_kiosk.sh
+++ scripts/start_kiosk.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Bildschirmschoner und Energiemanagement deaktivieren
-xset s noblank
+# Mauszeiger verstecken
+unclutter -idle 0.1 -root &
+
+# Bildschirmschoner und Energiesparmodus deaktivieren
 xset s off
+xset s noblank
 xset -dpms
 
-# Browser-Absturz-Meldungen unterdrücken
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/fred/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/fred/.config/chromium/Default/Preferences
-
-# Chromium im Kiosk-Modus mit High-DPI Skalierung starten
-chromium-browser \
-    --force-device-scale-factor=2.5 \
-    --kiosk \
-    --noerrdialogs \
-    --disable-infobars \
-    --autoplay-policy=no-user-gesture-required \
-    http://localhost:3000
+# Chromium im Kiosk-Modus starten
+# --window-size und --window-position helfen beim Waveshare Display
+chromium --kiosk --noerrdialogs --disable-infobars --window-size=1440,2560 --window-position=0,0 http://localhost:3000
