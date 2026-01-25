153869c0ade23b8856c2283e4cd5a85538b13b20
Casaluifred <113069241+Casaluifred@users.noreply.github.com>
2026-01-25 23:15:18 +0100

    Update start_kiosk.sh

--- scripts/start_kiosk.sh
+++ scripts/start_kiosk.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Mauszeiger verstecken
+# 1. Anzeige drehen
+# Da KMS aus ist, probieren wir 'HDMI-1' und als Fallback 'default'
+xrandr --output HDMI-1 --rotate right || xrandr --output default --rotate right
+
+# 2. Mauszeiger nach 0.1 Sek. Inaktivität verstecken
 unclutter -idle 0.1 -root &
 
-# Bildschirmschoner und Energiesparmodus deaktivieren
+# 3. Bildschirmschoner und Energiesparmodus aus
 xset s off
 xset s noblank
 xset -dpms
 
-# Chromium im Kiosk-Modus starten
-# Da wir per xrandr gedreht haben, ist die Auflösung nun 2560x1440
+# 4. Chromium im Kiosk-Modus starten
+# Fenstergröße jetzt 2560x1440 (durch Drehung)
 chromium --kiosk --noerrdialogs --disable-infobars --window-size=2560,1440 --window-position=0,0 http://localhost:3000
